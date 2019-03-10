a7c8d2ea4bfe4d214f1f862c8481ca557fce98b1
rolandn <roland.neyrinck@gmail.com>
2019-03-10 16:52:07 +0100

    Projet finalisé.

--- app/src/main/java/com/example/studentcomobile2019/MainActivity.java
+++ app/src/main/java/com/example/studentcomobile2019/MainActivity.java
@@ -1,26 +1,23 @@
 package com.example.studentcomobile2019;
 
-import android.content.Intent;
-import android.graphics.drawable.ColorDrawable;
+
 import android.os.StrictMode;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
+
+
 
 public class MainActivity extends AppCompatActivity {
 
     private String User;
     private String Password;
     private int nbrTentatives = 0;
-    private boolean TryEncode = true;
+    private boolean Authentif = true;
     String connectionResult;
     RESTClient restClient = new RESTClient();
-    String message;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,29 +34,35 @@ public class MainActivity extends AppCompatActivity {
                 StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder().detectNetwork().build());
                 connectionResult = restClient.callWebservice("/studentCo_rol_jquerry_17_oct/login/" + User + "/" + Password);
 
+                // Vérification que les deux champs user et mot de passe son setés
                 if(!User.toString().equals("")  && !Password.toString().equals("")){
-                    if (connectionResult.equals("true")) {
+                    // Il faut que la réponse du WS soit true + Anthentif = true (valeur initiale). Si pas true => on ne sait plus rien faire.
+                    if (connectionResult.equals("true") && Authentif == true) {
                         this.nbrTentatives = 0;
                         Toast.makeText(getApplicationContext(), getResources().getString(R.string.loginOK), Toast.LENGTH_LONG).show();
-                        TryEncode = true;
+                        Authentif = true;
 
-                        }
+                    }
+                    // Tant qu'on ne satifait pas à la connexion, on incrémente le nombre d'essai
                     else if(nbrTentatives  < 2){
                         nbrTentatives++;
                         Toast.makeText(getApplicationContext(), getResources().getString(R.string.retry), Toast.LENGTH_LONG).show();
                     }
+                    // Si on a dépassé le nombre d'essai => Authentif = false et là c'est foutu.
                     else {
-                        TryEncode = false;
+                        Authentif = false;
                         Toast.makeText(getApplicationContext(), getResources().getString(R.string.gloup), Toast.LENGTH_LONG).show();
                     }
 
                     break;
                 }
+                // Cas où l'utilisateur n'a rien encodé dans le champs utilisateur
                 else if ( User.toString().equals("")){
                     EditText  etUsername =  findViewById(R.id.leUsername);
                     etUsername.setError(getResources().getString(R.string.PasDeUsername));
 
                 }
+                // Cas où l'utilisateur n'a rien encodé dans le champs mot de passe
                 else if(Password.toString().equals("")){
                     EditText  etPassword =  findViewById(R.id.lePassword);
                     etPassword.setError(getResources().getString(R.string.PasDePassword));
@@ -67,31 +70,5 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu){
-        getMenuInflater().inflate(R.menu.main, menu);
-        ActionBar actionBar = getSupportActionBar();
-        actionBar.setDisplayShowHomeEnabled(true);
-        actionBar.setIcon(R.mipmap.ic_launcher);
-        return true;
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item){
-
-        int id = item.getItemId();
-
-        if(id == R.id.mainMenu_initLogin){
-            this.nbrTentatives = 0;
-            this.TryEncode = true;
-            Toast.makeText(getApplicationContext(), "Authentification réinitialisée", Toast.LENGTH_LONG).show();
-            return true;
-        }
-        if(id == R.id.mainMenu_quit){
-            finish();
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
